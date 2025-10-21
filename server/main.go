e710fc6a1cab62da21286dbd4ed9b14f1281d064
prasdud <prasdudwastaken@gmail.com>
2025-10-22 00:14:45 +0530

    feat(server ws): basic ws implementation for server side

---
+++ server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true }, // Allow all origins
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// Read message from client (blocks until message received)
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+		log.Printf("Received: %s", msg)
+
+		// Echo message back to client
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	log.Println("WebSocket server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
