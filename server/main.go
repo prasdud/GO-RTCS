7739f55e2b414dd0e01280efa64fc20c6b89cb33
prasdud <prasdudwastaken@gmail.com>
2025-10-26 21:30:43 +0530

    feat(server): added logging and featuresfor tracking client count

--- server/main.go
+++ server/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,16 +17,46 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize:  0,
 }
 
+var logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey {
+			if t, ok := a.Value.Any().(time.Time); ok {
+				return slog.Attr{
+					Key:   slog.TimeKey,
+					Value: slog.StringValue(t.Format("15:04:05")), //Mon Jan 2 15:04:05 MST 2006
+				} // Remove the time attribute
+			}
+		}
+		return a
+	},
+}))
+
+// make this a thread-safe structure in production, or add mutex
+var connectedClients = []string{}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrade error:", err)
 		return
 	}
-	defer conn.Close()
+
+	// custom defer to track client disconnection
+	defer func() {
+		logger.Info("Client disconnected", "address", r.RemoteAddr)
+		//log.Printf("Client disconnected: %s", r.RemoteAddr)
+		connectedClients = connectedClients[:len(connectedClients)-1]
+		conn.Close()
+	}()
+
+	// Track connected client
+	currentClient := r.RemoteAddr
+	connectedClients = append(connectedClients, currentClient)
+	log.Printf("Client connected: %s", currentClient)
 
 	for {
 		// Read message from client (blocks until message received)
+		log.Printf("Total active clients: %d", len(connectedClients))
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Read error:", err)
